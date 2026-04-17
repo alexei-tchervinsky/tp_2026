e9e27c605fca418f2cc8c56d5e7e451c36b877af
Winn-p <maerchuk010107@gmail.com>
2026-04-17 21:20:45 +0300

    2 commit for T4

--- maerchuk.danila/T4/circle.hpp
+++ maerchuk.danila/T4/circle.hpp
@@ -15,12 +15,12 @@ class Circle : public Shape {
 private:
     Point center;
     float radius;
-    
+
 public:
     Circle(const Point& center, float radius);
-    
+
     virtual ~Circle() override = default;
-    
+
     virtual float getArea() const override;
     virtual FrameRectangle getFrameRectangle() const override;
     virtual void move(const Point& newPos) override;
@@ -29,7 +29,7 @@ public:
     virtual std::string getName() const override;
     float getRadius() const;
     Point getCenter() const;
-    
+
 };
 
 #endif
--- maerchuk.danila/T4/square.hpp
+++ maerchuk.danila/T4/square.hpp
@@ -17,16 +17,16 @@ private:
     float side;
 public:
     Square(const Point& center, float side);
-    
+
     virtual ~Square() override = default;
-    
+
     virtual float getArea() const override;
     virtual FrameRectangle getFrameRectangle() const override;
     virtual void move(const Point& newPos) override;
     virtual void move(float dx, float dy) override;
     virtual Square* clone() const override;
     virtual std::string getName() const override;
-    
+
     float getSide() const;
     Point getCenter() const;
 };
