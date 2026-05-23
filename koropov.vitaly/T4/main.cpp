67dcf806f4af44078333897ed4eaa6dde42ac325
vitalyasq <sqvitalya@gmail.com>
2026-05-23 15:28:28 +0300

    Probably final T4

--- koropov.vitaly/T4/main.cpp
+++ koropov.vitaly/T4/main.cpp
@@ -1,43 +1,29 @@
 #include <iostream>
 #include <memory>
-
 #include "rectangle.h"
 #include "circle.h"
 #include "compositeshape.h"
-
 int main() {
     Point lb;
     lb.x = 0;
     lb.y = 0;
-
     Point rt;
     rt.x = 4;
     rt.y = 2;
-
     auto rect = std::make_shared<Rectangle>(lb, rt);
-
     Point center;
     center.x = 5;
     center.y = 5;
-
     auto circ = std::make_shared<Circle>(center, 3);
-
     CompositeShape comp;
-
     comp.addShape(rect);
     comp.addShape(circ);
-
     std::cout << comp.getName() << '\n';
     std::cout << comp.getArea() << '\n';
-
     Point compCenter = comp.getCenter();
-
     std::cout << compCenter.x << " "
         << compCenter.y << '\n';
-
     comp.scale(2);
-
     std::cout << comp.getArea() << '\n';
-
     return 0;
 }
