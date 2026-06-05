b37ecac6c72559faa515214517a6205a92d2d6b1
Olya Rizleyzeva <olyarizleyzeva@MacBook-Air-Olya.local>
2026-06-05 17:35:03 +0300

    fix: shorten lines in DataStruct.h

--- rizleytseva.olya/T2/DataStruct.h
+++ rizleytseva.olya/T2/DataStruct.h
@@ -3,39 +3,42 @@
 
 #include <iostream>
 #include <string>
+#include <complex>
 
 struct DataStruct {
-    // Поля структуры
-    unsigned long long key1;      // ULL HEX //  целое число (шестнадцатеричное число)
-    char key2;                     // CHR LIT // символ (символ в кавычках)
-    std::string key3;             // строка в кавычках
+    unsigned long long key1;
+    char key2;
+    std::string key3;
 
-    // Конструкторы
     DataStruct();
-    DataStruct(unsigned long long k1, char k2, const std::string& k3);
+    DataStruct(unsigned long long k1, char k2,
+               const std::string& k3);
     DataStruct(const DataStruct& other);
     DataStruct(DataStruct&& other) noexcept;
 
-    // Операторы присваивания
     DataStruct& operator=(const DataStruct& other);
     DataStruct& operator=(DataStruct&& other) noexcept;
 
-    // Операторы ввода/вывода
-    friend std::istream& operator>>(std::istream& is, DataStruct& data); //Читает строку, парсит, заполняет поля
-    friend std::ostream& operator<<(std::ostream& os, const DataStruct& data); //Выводит поля в форматированном виде
+    friend std::istream& operator>>(std::istream& is,
+                                    DataStruct& data);
+    friend std::ostream& operator<<(std::ostream& os,
+                                    const DataStruct& data);
 
     void swap(DataStruct& other) noexcept;
 
-//методы-парсеры
-//Каждый из них берёт строку, пытается превратить её в нужный тип и сохраняет в result.
 private:
-    static bool parseKey1(const std::string& valueStr, unsigned long long& result); //Шестнадцатеричное число
-    static bool parseKey2(const std::string& valueStr, char& result); //Символ в кавычках
-    static bool parseKey3(const std::string& valueStr, std::string& result); //Строку в кавычках
+    static bool parseKey1(const std::string& valueStr,
+                          unsigned long long& result);
+    static bool parseKey2(const std::string& valueStr,
+                          char& result);
+    static bool parseKey3(const std::string& valueStr,
+                          std::string& result);
 };
 
 struct DataStructComparator {
-    bool operator()(const DataStruct& lhs, const DataStruct& rhs) const;
+    bool operator()(const DataStruct& lhs,
+                    const DataStruct& rhs) const;
 };
 
 #endif
+
