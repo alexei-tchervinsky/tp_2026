a78cc4480111b6283ede18bb8441116251abdd67
mrchudak <gleb.ts2104@gmail.com>
2026-06-05 18:49:00 +0300

    fix build

--- tsuremichev.gleb/T3/geometry.cpp
+++ tsuremichev.gleb/T3/geometry.cpp
@@ -14,14 +14,29 @@ double getArea(const Polygon &poly)
   area = std::accumulate(poly.points.begin(), poly.points.end(), 0.0,
                          [&poly, n, base](double sum, const Point &p)
                          {
-                      size_t idx = &p - base;
-                      size_t next_idx = (idx + 1) % n;
-                      return sum +
-                          (poly.points[idx].x * poly.points[next_idx].y) -
-                          (poly.points[next_idx].x * poly.points[idx].y);
+                           size_t idx = &p - base;
+                           size_t next_idx = (idx + 1) % n;
+                           return sum + (poly.points[idx].x * poly.points[next_idx].y) - (poly.points[next_idx].x * poly.points[idx].y);
                          });
 
   return std::abs(area) / 2.0;
+}
+
+bool isIntersectingSegments(Point a, Point b, Point c, Point d)
+{
+  auto ccw = [](Point p1, Point p2, Point p3)
+  {
+    long long val = 1LL * (p3.y - p1.y) * (p2.x - p1.x) - 1LL * (p2.y - p1.y) * (p3.x - p1.x);
+    return (val > 0) ? 1 : ((val < 0) ? -1 : 0);
+  };
+
+  int r1 = ccw(a, b, c);
+  int r2 = ccw(a, b, d);
+  int r3 = ccw(c, d, a);
+  int r4 = ccw(c, d, b);
+
+  return (((r1 > 0 && r2 < 0) || (r1 < 0 && r2 > 0)) &&
+          ((r3 > 0 && r4 < 0) || (r3 < 0 && r4 > 0)));
 }
 
 bool isPointInsidePolygon(Point p, const Polygon &poly)
@@ -30,16 +45,13 @@ bool isPointInsidePolygon(Point p, const Polygon &poly)
   bool inside = false;
 
   const Point *base = poly.points.data();
-  std::for_each(poly.points.begin(), poly.points.end(),
-                [&](const Point &current)
+  std::for_each(poly.points.begin(), poly.points.end(), [&](const Point &current)
                 {
         size_t i = &current - base;
         Point next = poly.points[(i + 1) % n];
 
         if (((current.y > p.y) != (next.y > p.y)) &&
-            (p.x < (next.x - current.x) * (p.y - current.y) /
-            (double)(next.y - current.y) + current.x))
-        {
+            (p.x < (next.x - current.x) * (p.y - current.y) / (double)(next.y - current.y) + current.x)) {
             inside = !inside;
         } });
 
@@ -53,17 +65,11 @@ bool isIntersectingPolygons(const Polygon &p1, const Polygon &p2)
   const Point *base1 = p1.points.data();
   const Point *base2 = p2.points.data();
 
-  // 1. Проверяем пересечение границ
-  bool boundary_intersection = std::any_of(p1.points.begin(),
-                                           p1.points.end(),
-                                           [&](const Point &a)
+  bool boundary_intersection = std::any_of(p1.points.begin(), p1.points.end(), [&](const Point &a)
                                            {
         size_t i = &a - base1;
         Point b = p1.points[(i + 1) % n1];
-
-        return std::any_of(p2.points.begin(), p2.points.end(),
-                           [&](const Point& c)
-        {
+        return std::any_of(p2.points.begin(), p2.points.end(), [&](const Point& c) {
             size_t j = &c - base2;
             Point d = p2.points[(j + 1) % n2];
             return isIntersectingSegments(a, b, c, d);
