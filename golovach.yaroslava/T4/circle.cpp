b6c502f072e817ce9e4493dee0d23a5abb11b172
haglb <marshmelo120@gmail.com>
2026-06-05 12:15:28 +0300

    Add files via upload

--- golovach.yaroslava/T4/circle.cpp
+++ golovach.yaroslava/T4/circle.cpp
@@ -2,29 +2,27 @@
 #include <cmath>
 
 namespace golovach {
-  Circle::Circle(Point center, double radius) :
-    center_(center),
-    radius_(radius)
-  {}
+    Circle::Circle(Point center, double radius)
+        : center_(center), radius_(radius) {}
 
-  double Circle::getArea() const {
-    return M_PI * radius_ * radius_;
-  }
+    double Circle::getArea() const {
+        return M_PI * radius_ * radius_;
+    }
 
-  Point Circle::getCenter() const {
-    return center_;
-  }
+    Point Circle::getCenter() const {
+        return center_;
+    }
 
-  void Circle::move(double dx, double dy) {
-    center_.x += dx;
-    center_.y += dy;
-  }
+    void Circle::move(double dx, double dy) {
+        center_.x += dx;
+        center_.y += dy;
+    }
 
-  void Circle::scale(double factor) {
-    radius_ *= factor;
-  }
+    void Circle::scale(double factor) {
+        radius_ *= factor;
+    }
 
-  const char* Circle::getName() const {
-    return "CIRCLE";
-  }
+    const char* Circle::getName() const {
+        return "CIRCLE";
+    }
 }
--- golovach.yaroslava/T4/rectangle.hpp
+++ golovach.yaroslava/T4/rectangle.hpp
@@ -5,18 +5,18 @@
 #include "point.hpp"
 
 namespace golovach {
-  class Rectangle : public Shape {
-  public:
-    Rectangle(Point bottomLeft, Point topRight);
-    double getArea() const override;
-    Point getCenter() const override;
-    void move(double dx, double dy) override;
-    void scale(double factor) override;
-    const char* getName() const override;
-  private:
-    Point bottomLeft_;
-    Point topRight_;
-  };
+    class Rectangle : public Shape {
+    public:
+        Rectangle(Point bottomLeft, Point topRight);
+        double getArea() const override;
+        Point getCenter() const override;
+        void move(double dx, double dy) override;
+        void scale(double factor) override;
+        const char* getName() const override;
+    private:
+        Point bottomLeft_;
+        Point topRight_;
+    };
 }
 
 #endif
