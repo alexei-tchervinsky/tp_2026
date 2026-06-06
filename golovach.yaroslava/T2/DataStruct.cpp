60796fd5cf26755b4225ead682fabb6ba7725aae
golovach.yaroslava <marshmelo120@gmail.com>
2026-06-06 19:50:07 +0300

    fix: total reset and overwrite for variant 4

--- golovach.yaroslava/T2/DataStruct.cpp
+++ golovach.yaroslava/T2/DataStruct.cpp
@@ -28,7 +28,6 @@ std::istream& operator>>(std::istream& in, DblSciIO&& dest)
     return in;
   }
 
-  // Читаем мантиссу вручную для проверки формата (цифра.цифра)
   double mantissa = 0.0;
   in >> mantissa;
   if (!in)
@@ -166,7 +165,6 @@ std::ostream& operator<<(std::ostream& out, const DataStruct& src)
   }
   iofmtguard fmtguard(out);
 
-  // Приведение к научному формату, где мантисса от 1 до 10
   double val = src.key1;
   int exp = 0;
   if (val != 0.0)
@@ -176,173 +174,6 @@ std::ostream& operator<<(std::ostream& out, const DataStruct& src)
   }
 
   out << "(:key1 " << std::fixed << std::setprecision(1) << val << (exp >= 0 ? "e+" : "e") << exp;
-  out << ":key2 '" << src.key2 << "'";
-  out << ":key3 \"" << src.key3 << "\"";
-  out << ":)";
-  return out;
-}
-
-bool DataStruct::operator<(const DataStruct& other) const
-{
-  if (key1 != other.key1)
-  {
-    return key1 < other.key1;
-  }
-  if (key2 != other.key2)
-  {
-    return key2 < other.key2;
-  }
-  return key3.size() < other.key3.size();
-}
-
-bool DataStructComparator::operator()(const DataStruct& lhs,
-                                      const DataStruct& rhs) const
-{
-  return lhs < rhs;
-}#include "DataStruct.hpp"
-#include "iofmtguard.hpp"
-#include <limits>
-#include <iomanip>
-
-std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
-  {
-    return in;
-  }
-  char c = '0';
-  in >> c;
-  if (in && (c != dest.exp))
-  {
-    in.setstate(std::ios::failbit);
-  }
-  return in;
-}
-
-std::istream& operator>>(std::istream& in, DblSciIO&& dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
-  {
-    return in;
-  }
-  in >> std::scientific >> dest.ref;
-  return in;
-}
-
-std::istream& operator>>(std::istream& in, CharLitIO&& dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
-  {
-    return in;
-  }
-  in >> DelimiterIO{ '\'' };
-  in.get(dest.ref);
-  in >> DelimiterIO{ '\'' };
-  return in;
-}
-
-std::istream& operator>>(std::istream& in, StringIO&& dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
-  {
-    return in;
-  }
-  return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
-}
-
-std::istream& operator>>(std::istream& in, DataStruct& dest)
-{
-  std::istream::sentry sentry(in);
-  if (!sentry)
-  {
-    return in;
-  }
-
-  DataStruct input;
-  bool hasKey1 = false;
-  bool hasKey2 = false;
-  bool hasKey3 = false;
-
-  {
-    using sep = DelimiterIO;
-    using dbl = DblSciIO;
-    using chr = CharLitIO;
-    using str = StringIO;
-
-    in >> sep{ '(' } >> sep{ ':' };
-
-    for (int i = 0; i < 3 && in; ++i)
-    {
-      std::string keyName;
-      in >> keyName;
-
-      if (!in)
-      {
-        break;
-      }
-
-      if (keyName == "key1")
-      {
-        in >> dbl{ input.key1 } >> sep{ ':' };
-        if (in)
-        {
-          hasKey1 = true;
-        }
-      }
-      else if (keyName == "key2")
-      {
-        in >> chr{ input.key2 } >> sep{ ':' };
-        if (in)
-        {
-          hasKey2 = true;
-        }
-      }
-      else if (keyName == "key3")
-      {
-        in >> str{ input.key3 } >> sep{ ':' };
-        if (in)
-        {
-          hasKey3 = true;
-        }
-      }
-      else
-      {
-        in.setstate(std::ios::failbit);
-      }
-    }
-
-    if (in)
-    {
-      in >> sep{ ')' };
-    }
-  }
-
-  if (in && hasKey1 && hasKey2 && hasKey3)
-  {
-    dest = std::move(input);
-  }
-  else
-  {
-    in.setstate(std::ios::failbit);
-  }
-
-  return in;
-}
-
-std::ostream& operator<<(std::ostream& out, const DataStruct& src)
-{
-  std::ostream::sentry sentry(out);
-  if (!sentry)
-  {
-    return out;
-  }
-  iofmtguard fmtguard(out);
-
-  out << "(:key1 " << std::scientific << src.key1;
   out << ":key2 '" << src.key2 << "'";
   out << ":key3 \"" << src.key3 << "\"";
   out << ":)";
